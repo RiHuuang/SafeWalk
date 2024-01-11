2a4f0d0cec81c641eb7f1fad334f1778fe48f8df
RiHuuang <richardhuang.email@gmail.com>
2024-01-11 08:52:33 +0700

    Connect Firebase

--- safewalk/lib/main.dart
+++ safewalk/lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'login_page.dart';
 import 'register_page.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(MyApp());
 }
 
