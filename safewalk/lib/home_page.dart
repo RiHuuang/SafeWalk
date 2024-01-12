4adb5cf3d01e68b74e9d5c18941b9f89dff72729
RiHuuang <richardhuang.email@gmail.com>
2024-01-12 07:40:33 +0700

    ngerapihin

--- safewalk/lib/home_page.dart
+++ safewalk/lib/home_page.dart
@@ -3,7 +3,6 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:safewalk/contacts_page.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:safewalk/login_page.dart';
 import 'package:safewalk/settings_page.dart';
 
 
